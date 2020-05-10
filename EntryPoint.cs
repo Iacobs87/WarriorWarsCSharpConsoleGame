d6928f2f41f41d6e5b1e41d7cdd46f136027d395
Alexandru Iacoboae <alexiacoboae@gmail.com>
2020-05-10 08:39:49 +0300

    Warrior Game Source code

---
+++ EntryPoint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WarriorWarsCSharpConsoleGame.Enum;
+
+namespace WarriorWarsCSharpConsoleGame
+{
+    
+
+    class EntryPoint
+    {
+
+        static Random rng = new Random();
+        static void Main(string[] args)
+        {
+            Warrior goodGuy = new Warrior("Alex", Faction.GoodGuy);
+            Warrior badGuy = new Warrior("Andreea", Faction.BadGuy);
+
+            while (goodGuy.IsAlive && badGuy.IsAlive)
+            {
+
+                if (rng.Next(0,10) < 5)
+                {
+                    goodGuy.Attack(badGuy);
+                }
+                else
+                {
+                    badGuy.Attack(goodGuy);
+                }
+
+                Thread.Sleep(100);
+
+            }
+
+
+        }
+        
+    }
+}
